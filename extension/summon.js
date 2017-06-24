0a4b06926455c9977174730bab73ed13f08b4cba
Quang-Tri <qtrido@gmail.com>
2017-06-24 10:53:38 -0400

    button working

---
+++ extension/summon.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('#mainButton').addEventListener('click', main);
+});
+function main() {
+    alert("TEST");
+}
