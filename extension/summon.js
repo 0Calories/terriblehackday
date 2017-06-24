895b59a2e685148218ec989f29a41fcf3d4413bf
0Calories <aanan092@uottawa.ca>
2017-06-24 11:16:44 -0400

    Added summon.js

--- extension/summon.js
+++ extension/summon.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector('#mainButton').addEventListener('click', main);
 });
 function main() {
-    alert("TEST");
+    //alert("TEST");
+    chrome.windows.create({'url': 'annoyingwindow', 'type': 'popup'}, function(window) {
+  });
 }
